a72b510202cc4a6487c255173d292d36efeb66af
= <=>
2024-06-18 10:13:46 +0300

    add own lamport (now works)

--- rust-bitcoin-with-lamport/src/main.rs
+++ rust-bitcoin-with-lamport/src/main.rs
@@ -1,36 +1,16 @@
 mod key_generation;
 mod lamport;
 mod script_generation;
+mod own_lamport_sig;
 
 use bdk::bitcoin::secp256k1::Message;
 use key_generation::{generate_and_write_key_pairs, read_keys_from_files, sign_message, verify_signature};
+use own_lamport_sig::{signature, write_keys_to_file};
 use script_generation::build_script;
 //use lamport::{generate_keys, sign_message, verify_signature};
 fn main() {
-    //new keys generation
-
-    // generate_and_write_key_pairs(1);
-
-    // //check keys for valid
-
-    // // let prv1: Vec<String> = vec!("0130b0cddc89db7d13954956fe216bcf7881582e539bdffcaf31053f5459bc8f".to_string());
-    // // let pub1: Vec<String> = vec!("2d9f378b804c76e7c1fbf661e4d31fe8707bfba871f6baa239e8351fd35b7c22".to_string());
-    // // println!("{}", verify_encoded_key_pair(&prv1, &pub1));
-    
-    // //read keys from file
-
-    // let (private_keys, public_keys ) = read_keys_from_files("private_keys.txt", "public_keys.txt");
-
-
-    // let message = "123";
-    // let signature = sign_message(&message, &private_keys);
-    // let verify  = verify_signature(&message, &signature, &public_keys);
-    // println!("msg: {}, sig {:#?}, verified {} ", message, signature, verify);
-
-   
-
-    let script = build_script();
-    println!("Generated Script: {}", script);
-
-
+    //let _ = write_keys_to_file(1);
+    let sig = signature(false);
+    let res = own_lamport_sig::verify_signature(false, sig.clone());
+    println!("{:#?}, {}", sig, res);
 }
---
+++ rust-bitcoin-with-lamport/src/own_lamport_sig.rs
@@ -0,0 +1,83 @@
+use bdk::bitcoin::network::message;
+use bitcoin::io::Error;
+use rand::distributions::Alphanumeric;
+use rand::{thread_rng, Rng};
+use std::fs::File;
+use std::io::{Write, BufWriter, BufReader, BufRead};
+use sha2::{Sha256, Digest};
+use hex;
+
+pub fn get_sha256(data:String)->String{
+    let mut hasher = Sha256::new();
+    hasher.update(data);
+    hex::encode(hasher.finalize())
+}
+
+pub fn key_generation()-> (String, String){
+    let pub_key:String;
+    let prv_key:String;
+
+    let mut rng = thread_rng();
+    prv_key = get_sha256((0..16)
+    .map(|_| rng.sample(Alphanumeric) as char)
+    .collect());
+    pub_key = get_sha256(prv_key.clone());
+    (prv_key, pub_key)
+}
+pub fn write_keys_to_file(message_len: i8)->Result<(), Error>{
+    let mut private_file = BufWriter::new(File::create("private_keys.txt")?);
+    let mut public_file = BufWriter::new(File::create("public_keys.txt")?);
+
+    for _ in 0..message_len*2{
+        let (prv_key, pub_key) = key_generation();
+        writeln!(private_file, "{}", prv_key)?;
+        writeln!(public_file, "{}", pub_key)?;
+    }
+
+    Ok(())
+}
+
+pub fn read_keys_from_files(private_key_file: &str, public_key_file: &str) -> (Vec<String>, Vec<String>) {
+    let private_file = File::open(private_key_file).expect("Unable to open private key file");
+    let public_file = File::open(public_key_file).expect("Unable to open public key file");
+
+    let private_keys: Vec<String> = BufReader::new(private_file)
+        .lines()
+        .filter_map(Result::ok)
+        .filter(|line| !line.starts_with("Private Key"))
+        .collect();
+
+    let public_keys: Vec<String> = BufReader::new(public_file)
+        .lines()
+        .filter_map(Result::ok)
+        .filter(|line| !line.starts_with("Public Key"))
+        .collect();
+
+    (private_keys, public_keys)
+}
+
+pub fn signature(message: bool)->Vec<String>{
+    let (private_keys, _) = read_keys_from_files("private_keys.txt", "public_keys.txt");
+    let mut sig: Vec<String> = vec!();
+    if message {
+        sig.push(private_keys[1].clone());
+    }else {
+        sig.push(private_keys[0].clone());
+    }
+    sig
+}
+
+pub fn verify_signature(message: bool, sig: Vec<String>)-> bool{
+    let (_, public_keys) = read_keys_from_files("private_keys.txt", "public_keys.txt");
+    for pk in sig.iter() {
+        let check = get_sha256(pk.to_string());
+        let int_bool = match message {
+            true => 1,
+            false => 0,
+        };
+        if check != public_keys[int_bool]{
+            return false
+        }
+    }
+    true
+}
